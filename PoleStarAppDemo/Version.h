3f86ac76f4c9b76c3aa93501f6c2b386cea75148
Honoré Bizagwira <honore.bizagwira@polestar.eu>
2019-11-29 10:58:48 +0100

    Removing our owner test keys

---
+++ PoleStarAppDemo/Version.h
@@ -0,0 +1,23 @@
+//
+// Version.h
+//
+//
+
+
+
+
+#ifndef SDK_VERSIONS
+#define SDK_VERSIONS
+
+
+
+#define  INDOORLOCATION   "INDOORLOCATION : 1.0.4"
+#define  MAPBOXIOSSDK   "MAPBOX-IOS-SDK : 5.5.0"
+#define  MAPWIZESDK   "MAPWIZESDK : 3.0.10"
+#define  MAPWIZEUI   "MAPWIZEUI : 2.0.3"
+#define  NAOSDK   "NAOSDK : 4.7.3"
+#define  SSZIPARCHIVE   "SSZIPARCHIVE : 2.2.2"
+
+
+
+#endif // SDK_VERSIONS
